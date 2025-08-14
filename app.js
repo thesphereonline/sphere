03ca949abb9a0f466ab7f7c66a98382ca1120e8b
0xygengg <joe@izatt.co.uk>
2025-08-14 03:02:08 +0100

    refresh

---
+++ app.js
@@ -0,0 +1,3 @@
+document.getElementById("root").backgroundColor = "black";
+
+
--- core/types/state.go
+++
@@ -1,8 +0,0 @@
-package types
-
-type State struct {
-	Balances map[string]uint64            // address → balance
-	Tokens   map[string]map[string]uint64 // tokenAddr → (address → balance)
-	NFTs     map[string]string            // tokenId → owner
-	Metadata map[string]string            // tokenId → URI
-}
