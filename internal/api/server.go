8c67a0904b5ea13626b35ca328ae089f31cc9ae8
0xygengg <joe@izatt.co.uk>
2025-08-28 17:43:01 +0100

    add cors

--- internal/api/server.go
+++ internal/api/server.go
@@ -1,19 +1,29 @@
-// internal/api/server.go
 package api
 
 import (
 	"sphere/internal/core"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
 func StartServer(bc *core.Blockchain) {
 	app := fiber.New()
 
+	// Enable CORS
+	app.Use(cors.New(cors.Config{
+		AllowOrigins:     "https://www.thesphere.online", // your frontend
+		AllowMethods:     "GET,POST,OPTIONS",
+		AllowHeaders:     "Content-Type",
+		AllowCredentials: true,
+	}))
+
+	// Get all blocks
 	app.Get("/blocks", func(c *fiber.Ctx) error {
 		return c.JSON(bc.Chain)
 	})
 
+	// Submit transaction (creates a block)
 	app.Post("/tx", func(c *fiber.Ctx) error {
 		tx := new(core.Transaction)
 		if err := c.BodyParser(tx); err != nil {
@@ -23,5 +33,6 @@ func StartServer(bc *core.Blockchain) {
 		return c.JSON(block)
 	})
 
+	// Start server
 	app.Listen(":8080")
 }
